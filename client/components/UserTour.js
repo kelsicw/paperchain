8ef5ca01a047d17488ab1e5b31eb9cf6dc224808
Kelsi Webb <kelsicw@gmail.com>
2024-01-28 21:03:32 -0500

    updates text for user tour and updates styling

--- client/components/UserTour.js
+++ client/components/UserTour.js
@@ -10,12 +10,13 @@ const UserTour = () => {
       const newDriverObj = driver({
         showProgress: true,
         overlayColor: 'rgba(214, 214, 214, 0.75)',
+        popoverClass: 'my-custom-popover-class',
         steps: [
           {
             element: '#menu-button',
             popover: {
               title: 'Get Started',
-              description: `Click the plus sign to create your paperchain countdown, set your intention, and add encouraging reminders. Then, click 'Next'!`,
+              description: `Click the plus sign to create a new PaperChain countdown, set your intention, and add encouraging reminders.`,
               onNextClick: () => {
                 document.getElementById('menu-button').checked = true;
                 newDriverObj.moveNext();
@@ -27,29 +28,28 @@ const UserTour = () => {
           {
             element: '#create-new-chain-form',
             popover: {
-              title: 'Import the Library',
-              description:
-                'It works the same in vanilla JavaScript as well as frameworks.',
-              side: 'bottom',
+              title: 'Start A New Chain',
+              description: `Enter the number of chain links you need on your chain, then click 'Create Chain'.`,
+              side: 'right',
               align: 'start',
             },
           },
           {
             element: '#set-intention-form',
             popover: {
-              title: 'Importing CSS',
+              title: 'Set Your Intention',
               description:
-                'Import the CSS which gives you the default styling for popover and overlay.',
-              side: 'bottom',
+                'Setting an intention to focus on throughout your countdown will help you stay focused on the big picture.',
+              side: 'top',
               align: 'start',
             },
           },
           {
             element: '#add-reminder-form',
             popover: {
-              title: 'Create Driver',
+              title: 'Add Reminders',
               description:
-                'Simply call the driver function to create a driver.js instance',
+                'Add some kind words and encouragement for the moments when you need support along the way!',
               onNextClick: () => {
                 document.getElementById('menu-button').checked = false;
                 newDriverObj.moveNext();
@@ -61,9 +61,9 @@ const UserTour = () => {
           {
             element: '#chain',
             popover: {
-              title: 'Start Tour',
+              title: 'Update Your Chain',
               description:
-                'Call the drive method to start the tour and your tour will be started.',
+                'Click your PaperChain to remove a link. Each link brings you one step closer to your goal!',
               side: 'left',
               align: 'start',
             },
@@ -71,18 +71,16 @@ const UserTour = () => {
           {
             element: '#current-week',
             popover: {
-              title: 'More Configuration',
-              description:
-                'Look at this page for all the configuration options you can pass.',
+              title: 'Keep Track Of Your Progress',
+              description: `See how far you've come and how much you have left to go right here in your tracker.`,
               side: 'right',
               align: 'start',
             },
           },
           {
             popover: {
-              title: 'Happy Coding',
-              description:
-                'And that is all, go ahead and start adding tours to your applications.',
+              title: `You're all set!`,
+              description: `Let the countdown begin! If you need some encouragement along the way, click the "Need a reminder?" button to cheer yourself on. You've got this!`,
             },
           },
         ],
